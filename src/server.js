48644de8be1248e884362dc2606091e29a6301a0
Donald Le <ledinhcuong99@gmail.com>
2023-11-05 07:25:17 +0700

    set app to use environment variables instead of hard values

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3001;
+const port = process.env.APP_PORT;
 
 const postRoutes = require('./routes/postRoutes');
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
 mongoose
-  .connect('mongodb://localhost:27017/posts', {
+  .connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -20,11 +23,11 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/posts', postRoutes);
+app.use(`/${MONGO_DB_NAME}`, postRoutes);
 
 app.on('db-connected', () => {
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-    console.log(`http://localhost:${port}/posts`);
+    console.log(`http://localhost:${port}/${MONGO_DB_NAME}`);
   });
 });
